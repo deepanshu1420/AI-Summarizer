55e29acf4266ddf213f5ccd3e2e5b11567d9f432
Deepanshu Sharma <deepanshusharma1420@gmail.com>
2026-07-28 03:39:53 +0530

    Added homepage refresh on Summarizer logo click!

--- src/services/article.js
+++ src/services/article.js
@@ -25,7 +25,7 @@ export const articleApi = createApi({
         params: {
           url: articleUrl,
           method: 'textrank',
-          sentences: 10
+          sentences: 5
         }
       })
     })
